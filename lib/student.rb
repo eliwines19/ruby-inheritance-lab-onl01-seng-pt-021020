34e82274b082621c02e63dee2211a4919506d885
Elijah wines <elijahwines84@yahoo.com>
2020-03-10 19:15:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,14 @@
 class Student < User
 
+  attr_accessor :knowledge
+
 
   def initialize
     @knowledge = []
   end
 
   def learn(string)
-    @knowledge << string
-  end
-
-  def knowledge
-    @knowledge
+    self.knowledge << string
   end
 
 end
